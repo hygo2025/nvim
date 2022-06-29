18071052b91aab1b87963530c881b7334dfeeb25
Hygo <hygo2025@gmail.com>
2022-06-29 15:56:14 -0300

    Arch config

---
+++ install_dependencies.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+sudo pacman -Sy curl wget unzip go ripgrep fd tilix python-pip;
+
+cd ~/;
+git clone https://aur.archlinux.org/yay.git;
+cd yay;
+makepkg -si;
+
+yay -Sy neovim visual-studio-code-bin google-chrome chrome-gnome-shell dbeaver discord;
+
+pip install pynvim;
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash;
+
+mkdir ~/.config/nvim;
+git clone https://github.com/hygo2025/nvim.git ~/.config/nvim;
+
+nvim --headless +PlugInstall +qa;
+
+mkdir $HOME/.local/share/fonts;
+wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip;
+unzip $HOME/.local/share/fonts/Hack.zip;
+rm Hack.zip;
+fc-cache;
+
+cd ~/Documents;
+wget https://github.com/RinCat/RTL88x2BU-Linux-Driver/archive/master.zip;
+unzip master.zip;
+cd RTL88x2BU-Linux-Driver-master;
+make -j $(nproc);
+sudo make install;
+sudo modprobe 88x2bu rtw_switch_usb_mode=1;
+
